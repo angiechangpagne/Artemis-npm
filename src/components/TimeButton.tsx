83a08050d73963e21b91a5b066eec464054b355b
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-01-25 19:39:44 -0800

    Test of Time

---
+++ src/components/TimeButton.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import msgToBackground from '../Utility/msgToBackground'
+
+interface iProps{
+  timeStamp: any,
+  history: string[],
+  index: number,
+}
+
+const TimeButton: React.FC<iProps> = ({ timeStamp, history, index }) => {
+
+  const [isHovered, setHovered] = React.useState(false);
+  function toggleTime(index: number) {
+    msgToBackground(
+      "contentScript",
+      "rerenderDOM",
+      response => console.log(response),
+      history[index]
+    );
+
+  }
+  const hovered = isHovered ? 'Click Me!' : timeStamp.format('h:mm:ss');
+
+  return (
+    <div>
+      <button className="time-button"
+        onClick={() => toggleTime(index)}
+        onMouseOver={() => setHovered(true)}
+        onMouseOut={() =>setHovered(false)}>
+          {hovered}
+        </button>
+    </div>
+  );
+};
+
+export default TimeButton;
