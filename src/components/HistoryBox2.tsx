72f92921611dd36015615df4e22e5c6058c058b3
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-01-25 19:14:50 -0800

    ET Phone Home

--- src/components/HistoryBox2.tsx
+++ src/components/HistoryBox2.tsx
@@ -2,5 +2,18 @@ import * as React from 'react'
 import TimeButton from '../components/TimeButton'
 
 interface iProps {
-
+  i: number;
+  history: string[];
+  isToggle: (i: number) => void;
 }
+
+const HistoryBox: React.FC<iProps> = ({ isToggle, i, history }) => {
+  return (
+    <div id="queryBox" onClick={() => isToggle(i)}>
+      <div>Query {i+1} </div>
+      <TimeButton timeStamp={{ temp: "temp" }} histroy={history} index={i} />
+    </div>
+  )
+}
+
+export default HistoryBox;
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Home : React.FC = () => {
+  return (
+    <div id='home-component'>
+      <h1> ARTEMIS </h1>
+      <p>GraphQL query has not been detected yet. </p>
+      <div className="loarder">Loading...</div>
+    </div>
+  )
+}
+ export default Home;
