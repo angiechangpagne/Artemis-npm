04d9c8f1fa9ab11547f5ef8780b3d03b2f0ff7be
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-01-25 18:59:49 -0800

    history

---
+++ src/components/HistoryBox.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { Accordion } from 'semantic-ui-react'
+import TimeButton from '../components/TimeButton'
+
+interface iProps {
+  key: number,
+  timeStamp: any,
+  history: string[],
+  activeIndex: number,
+  i:number,
+  isToggle: (i: number) => void,
+  setActiveIndex: (i: number) => void;
+}
+
+const HistoryBox: React.FC<iProps> = ({ timeStamp, activeIndex, setActiveIndex, history, i, isToggle }) => {
+  return ( 
+    <React.Fragment> 
+      <Accordion.Title
+        active={activeIndex === i}
+        index={i}
+        onClick={() => {
+          const newIndex = activeIndex === i ? -1 : i
+          setActiveIndex(newIndex);
+        }}
+        >
+          <div id="queryBox" onClick={() => isToggle(i)}>
+            <div> Query {i+1}</div>
+            <TimeButton timeStamp={timeStamp} history={history} index={i} />
+          </div>
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex ===i }>
+          <p> Performance</p>
+        </Accordion.Content>
+    </React.Fragment>
+  )
+}
+
+
+export default HistoryBox;
---
+++ src/components/HistoryBox2.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import TimeButton from '../components/TimeButton'
+
+interface iProps {
+
+}
